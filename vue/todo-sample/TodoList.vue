168706f68f1ecc8e46cc20ae996ccd32021f5b14
sunny <32916757+youngsunWoo@users.noreply.github.com>
2021-01-26 11:58:58 +0900

    Add files via upload

---
+++ vue/todo-sample/TodoList.vue
@@ -0,0 +1,75 @@
+<template>
+    <section>
+        <transition-group name="list" tag="ul">
+            =<li v-for="(todoItem, index) in toDoItems" class="shadow"  v-bind:key="todoItem">
+                <i class="checkBtn fas fa-check" aria-hidden="true"></i>
+                {{ todoItem }}
+                <span class="removeBtn" type="button" @click="removeTodo(todoItem,index)">
+                <i class="far fa-trash-alt" aria-hidden="true"></i>
+                </span>
+            </li>
+    </transition-group>
+    </section>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            toDoItems : []
+        }
+    },
+    created(){
+        if(localStorage.length>0){
+            for(var i= 0;i<localStorage.length;i++){
+                this.toDoItems.push(localStorage.key(i));
+                console.log(localStorage.key(i));
+            }
+        }
+    },
+    methods:{
+        removeTodo(todoItem,index){
+            localStorage.clear(todoItem)
+            this.toDoItems.splice(index,1)  
+            console.log(todoItem, index)
+        }
+    }
+    
+}
+</script>
+
+
+<style scoped>
+  ul {
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+  }
+  li {
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+  }
+  .checkBtn {
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
+  }
+  .removeBtn {
+    margin-left: auto;
+    color: #de4343;
+  }
+  .list-enter-active, .list-leave-active {
+    transition: all 1s;
+  }
+  .list-enter, .list-leave-to {
+    opacity: 0;
+    transform: translateY(30px);
+  }
+</style>
